4a2a66df16105a487d900e6e8dd98f6c2ac5ac9e
Riley <40878107+rileyloudon@users.noreply.github.com>
2023-04-07 22:43:26 -0500

    add layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import Header from '@/Components/Header/Header';
+import Footer from '@/Components/Footer/Footer';
+import { Lato } from 'next/font/google';
+import './global.css';
+
+export const metadata = {
+  title: 'Ratings',
+  description: 'View TMDB Rating Graphs',
+  themeColor: '#161616',
+  openGraph: {
+    title: 'Ratings',
+    images: [
+      {
+        url: '/og-image.png',
+      },
+    ],
+  },
+  icons: {
+    icon: '/favicon.svg',
+  },
+};
+
+const lato = Lato({ weight: '400', subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body
+        className={lato.className}
+        style={{ backgroundColor: 'var(--background)' }}
+      >
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
