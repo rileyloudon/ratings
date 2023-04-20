166c46560ed2e68b7a6c8f92e4353107366cacbf
Riley <40878107+rileyloudon@users.noreply.github.com>
2023-04-19 23:37:54 -0500

    add Graphs

---
+++ app/actor/[actorId]/Graphs.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { useState } from 'react';
+import { Movie, TvShow } from '@/utils/types';
+import LineGraph from '@/components/LineGraph/LineGraph';
+import styles from './Graphs.module.css';
+
+interface GraphsProps {
+  credits: (Movie | TvShow)[];
+}
+
+const Graphs = ({ credits }: GraphsProps) => {
+  const creditsToDisplay = window.innerWidth > 400 ? 10 : 5;
+
+  const [displayedData, setDisplayedData] = useState<(Movie | TvShow)[]>(
+    credits.slice(0, creditsToDisplay)
+  );
+
+  const handlePrevClick = () => {
+    const endingValue = credits.findIndex(
+      (obj) => obj.id === displayedData[0].id
+    );
+
+    setDisplayedData(
+      credits.slice(
+        endingValue - creditsToDisplay >= 0
+          ? endingValue - creditsToDisplay
+          : 0,
+        endingValue
+      )
+    );
+  };
+
+  const handleNextClick = () => {
+    const startingValue = credits.findIndex(
+      (obj) => obj.id === displayedData[displayedData.length - 1].id
+    );
+
+    setDisplayedData(
+      credits.slice(
+        startingValue + 1,
+        startingValue + creditsToDisplay + 1 <= credits.length
+          ? startingValue + creditsToDisplay + 1
+          : credits.length
+      )
+    );
+  };
+
+  return (
+    <div className={styles.credits}>
+      <p>Most Popular Credits</p>
+      <div className={styles.nav}>
+        <button
+          disabled={displayedData[0].id === credits[0].id}
+          type='button'
+          onClick={handlePrevClick}
+        >
+          Previous {creditsToDisplay}
+        </button>
+        <button
+          disabled={
+            displayedData[displayedData.length - 1].id ===
+            credits[credits.length - 1].id
+          }
+          type='button'
+          onClick={handleNextClick}
+        >
+          Next {creditsToDisplay}
+        </button>
+      </div>
+      <LineGraph
+        data={displayedData}
+        xAxisDataKey='name'
+        xAxisLabel='Credit Name'
+        allowClick
+      />
+    </div>
+  );
+};
+export default Graphs;
