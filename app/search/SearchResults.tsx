1a75896e2d6e62b135e1f6ce51544dd1f21466ba
Riley <40878107+rileyloudon@users.noreply.github.com>
2023-04-23 22:44:51 -0500

    add SearchResults

---
+++ app/search/SearchResults.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import {
+  SearchResultMovie,
+  SearchResultPerson,
+  SearchResultTv,
+} from '@/utils/types';
+import NoPoster from '@/components/NoPoster/NoPoster';
+import { useRouter, useSearchParams } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './SearchResults.module.css';
+
+const SearchResults = ({
+  results,
+  pageNumber,
+  totalPages,
+}: {
+  results: (SearchResultTv | SearchResultMovie | SearchResultPerson)[];
+  pageNumber: number;
+  totalPages: number;
+}) => {
+  const router = useRouter();
+  const search = useSearchParams();
+  const query = search.get('q');
+  const page = Number(search.get('page')) || 1;
+
+  const renderSearchResults = (): JSX.Element => {
+    return (
+      <>
+        <div className={styles.results}>
+          {results.map((item) => {
+            const imgPath =
+              'profile_path' in item ? item.profile_path : item.poster_path;
+            const name = 'name' in item ? item.name : item.title;
+
+            let link = 'movie';
+            if (item.media_type === 'tv') link = 'tvshow';
+            else if (item.media_type === 'person') link = 'actor';
+
+            return (
+              <Link
+                href={`/${link}/${item.id}`}
+                key={item.id}
+                className={styles.item}
+              >
+                {imgPath !== null ? (
+                  <Image
+                    src={`https://image.tmdb.org/t/p/w300/${imgPath}`}
+                    alt={`${name} Poster`}
+                    width={300}
+                    height={250}
+                  />
+                ) : (
+                  <NoPoster />
+                )}
+                <p>{name}</p>
+              </Link>
+            );
+          })}
+        </div>
+        <div className={styles['page-selector']}>
+          <button
+            disabled={pageNumber === 1}
+            type='button'
+            onClick={() => router.push(`/search?q=${query}&page=${page - 1}`)}
+          >
+            Previous Page
+          </button>
+          <button
+            disabled={pageNumber === totalPages}
+            type='button'
+            onClick={() => router.push(`/search?q=${query}&page=${page + 1}`)}
+          >
+            Next Page
+          </button>
+        </div>
+      </>
+    );
+  };
+
+  return <div className={styles.container}>{renderSearchResults()}</div>;
+};
+
+export default SearchResults;
