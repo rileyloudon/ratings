660e32cb7ceac334583b111a3dbc11cb912c5de5
Riley <40878107+rileyloudon@users.noreply.github.com>
2023-06-25 23:57:30 -0500

    update to use HorizontalScroll

--- components/CastCard/CastCard.tsx
+++ components/CastCard/CastCard.tsx
@@ -1,30 +1,40 @@
+'use client';
+
+import HorizontalScoll from '../HorizontalScroll/HorizontalScoll';
 import Link from 'next/link';
 import { Credit } from '../../utils/types';
-import styles from './CastCard.module.css';
+import { useRef } from 'react';
 import Image from 'next/image';
+import styles from './CastCard.module.css';
 
-const CastCard = ({ cast }: { cast: Credit[] }) => (
-  <div className={styles.castCards}>
-    {cast.map((person) =>
-      person.profile_path !== null ? (
-        <Link
-          prefetch={false}
-          href={`/actor/${person.id}`}
-          key={person.id.toString() + person.character}
-          className={styles.card}
-        >
-          <Image
-            src={`https://image.tmdb.org/t/p/w185${person.profile_path}`}
-            alt={`${person.name} Poster`}
-            width={185}
-            height={277}
-          />
-          <p className={styles.name}>{person.name}</p>
-          <p className={styles.character}>{person.character}</p>
-        </Link>
-      ) : null
-    )}
-  </div>
-);
+const CastCard = ({ cast }: { cast: Credit[] }) => {
+  const cardRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <HorizontalScoll containerRef={cardRef} maxWidthPerItem={185}>
+      <div className={styles.castCards} ref={cardRef}>
+        {cast.map((person) =>
+          person.profile_path !== null ? (
+            <Link
+              prefetch={false}
+              href={`/actor/${person.id}`}
+              key={person.id.toString() + person.character}
+              className={styles.card}
+            >
+              <Image
+                src={`https://image.tmdb.org/t/p/w185${person.profile_path}`}
+                alt={`${person.name} Poster`}
+                width={185}
+                height={277}
+              />
+              <p className={styles.name}>{person.name}</p>
+              <p className={styles.character}>{person.character}</p>
+            </Link>
+          ) : null
+        )}
+      </div>
+    </HorizontalScoll>
+  );
+};
 
 export default CastCard;
