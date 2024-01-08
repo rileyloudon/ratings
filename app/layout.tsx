26d566091f4f22e679c3658f6d130326a7c809fd
Riley <40878107+rileyloudon@users.noreply.github.com>
2024-01-07 20:13:48 -0600

    update themeColor to use viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,21 @@
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import { Lato } from 'next/font/google';
+import { Metadata, Viewport } from 'next';
 import './global.css';
 
-export const metadata = {
+export const metadata: Metadata = {
   metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
   title: { template: '%s - Ratings', default: 'Ratings' },
   description: 'View TMDB Rating Graphs',
-  themeColor: '#161616',
   openGraph: {
     title: { template: '%s - Ratings', default: 'Ratings' },
     description: 'View TMDB Rating Graphs',
   },
+};
+
+export const viewport: Viewport = {
+  themeColor: '#161616',
 };
 
 const lato = Lato({ weight: '400', subsets: ['latin'] });
